7cc8cdbd568a7c5419e2ee539d77189150ad0087
JoaquinKorol <joaquinkorol@gmail.com>
2024-08-20 20:59:18 -0300

    Cree Clase Clientes

---
+++ Models/Clientes.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+
+namespace KoruCosmetica.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Mail { get; set; }
+        public string Telefono { get; set; }
+        public ICollection<Turnos>? Turnos{ get; }
+        public Clientes()
+        {
+            
+        }
+    }
+}
