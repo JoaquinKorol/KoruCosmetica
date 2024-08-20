b8260ad033b4bf4d5b5761b239e93b34bcb7db7d
JoaquinKorol <joaquinkorol@gmail.com>
2024-08-20 20:59:51 -0300

    Cree Clase Profesionales

---
+++ Models/Profesionales.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KoruCosmetica.Models
+{
+    public class Profesionales
+    {
+        [Key]
+        public int ProfesionalID {  get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public ICollection<Turnos> Turnos { get; }
+        public Profesionales()
+        {
+            
+        }
+    }
+}
