77d8c2c6048bc88d8cb7d785d143e6cad1dcd194
JoaquinKorol <joaquinkorol@gmail.com>
2024-08-20 20:59:38 -0300

    Cree Clase Pagos

---
+++ Models/Pagos.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KoruCosmetica.Models
+{
+    public class Pagos
+    {
+        [Key]
+        public int PagosID { get; set; }
+        public DateTime? Fecha { get; set; }
+        public float? Monto { get; set; }
+        public string Metodo_Pago { get; set; }
+        public string Estado { get; set; }
+
+        // Claves foráneas
+        public int? ClienteID { get; set; }
+        public int? TurnoID { get; set; }
+        public Clientes Cliente { get; set; }
+        public Turnos Turno { get; set; }
+
+        public Pagos()
+        {
+            
+        }
+    }
+}
