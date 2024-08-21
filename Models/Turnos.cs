a7a36023da3828d37e6f24ac7bcbb2d35521a2ac
JoaquinKorol <joaquinkorol@gmail.com>
2024-08-20 21:00:05 -0300

    Cree Clase Turnos

---
+++ Models/Turnos.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KoruCosmetica.Models
+{
+    public class Turnos
+    {
+        [Key]
+        public int TurnosId { get; set; }
+        public DateOnly Fecha { get; set; }
+        public TimeOnly Hora { get; set; }
+        public string? Estado { get; set; }
+
+        // Foreign Keys
+        // Las Foreign Keys se definen como nullable para que no sean obligatorias
+        [ForeignKey("ClienteID")]
+        public int? ClienteID { get; set; }
+        [ForeignKey("ServicioID")]
+        public int? ServicioID { get; set; }
+        [ForeignKey("ProfesionalID")]
+        public int? ProfesionalID { get; set; }
+
+        // Propiedades de navegación opcionales si utilizas Entity Framework
+        public Clientes? Cliente { get; set; }
+        public Servicios? Servicio { get; set; }
+        public Profesionales? Profesional { get; set; }
+
+        public Turnos()
+        {
+            
+        }
+    }
+}
