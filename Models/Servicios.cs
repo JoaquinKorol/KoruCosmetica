3b7998fc7b71a5d23903680238014a837111f229
JoaquinKorol <joaquinkorol@gmail.com>
2024-08-20 21:00:30 -0300

    Cree Clase Servicios

---
+++ Models/Servicios.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KoruCosmetica.Models
+{
+    public class Servicios
+    {
+        [Key]
+        public int ServicioID { get; set; }
+        public string Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public int Duracion {  get; set; }
+
+        public ICollection<Turnos> Turnos { get; }
+        public Servicios()
+        {
+            
+        }
+    }
+}
