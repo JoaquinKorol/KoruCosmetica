5ab1105812717354aed6092e2aa80edcdf249646
JoaquinKorol <joaquinkorol@gmail.com>
2024-08-21 16:00:21 -0300

    Agregue funcionalidad mostrar Turno al Cliente

--- Controllers/ClientesTurnoController.cs
+++ Controllers/ClientesTurnoController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using KoruCosmetica.Models.ViewModel;
 using KoruCosmetica.Models;
 using KoruCosmetica.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace KoruCosmetica.Controllers
 {
@@ -15,20 +16,37 @@ namespace KoruCosmetica.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(Turnos turnos)
         {
+            if (TempData["ClienteID"] is int clienteId)
+            {
+                turnos.ClienteID = clienteId;
+                var dataTurno = (from c in _context.Clientes
+                            join t in _context.Turnos on c.ClienteID equals t.ClienteID
+                            join s in _context.Turnos on t.ServicioID equals s.ServicioID
+                            where c.ClienteID == clienteId
+                            select new ClientesTurnos
+                            {
+                                Nombre = c.Nombre,
+                                Apellido = c.Apellido,
+                                Fecha = t.Fecha.ToDateTime(TimeOnly.MinValue),
+                                Hora = t.Hora,
+                                Servicio = s.Servicio.Nombre
+                            }).ToList();
+
+                return View(dataTurno);
+            }
             var data = (from c in _context.Clientes
                         join t in _context.Turnos on c.ClienteID equals t.ClienteID
-                        join s in _context.Turnos on t.ServicioID equals s.ServicioID // Corregido aquí
+                        join s in _context.Turnos on t.ServicioID equals s.ServicioID
                         select new ClientesTurnos
                         {
                             Nombre = c.Nombre,
                             Apellido = c.Apellido,
-                            Fecha = t.Fecha.ToDateTime(TimeOnly.MinValue), // Si 'Fecha' ya es DateTime, no necesitas ToDateTime
+                            Fecha = t.Fecha.ToDateTime(TimeOnly.MinValue),
                             Hora = t.Hora,
-                            Servicio = s.Servicio.Nombre // Usar la propiedad 'Nombre' del modelo 'Servicios'
+                            Servicio = s.Servicio.Nombre
                         }).ToList();
-
             return View(data);
         }
     }
