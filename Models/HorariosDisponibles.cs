3249c80a34447f42cfc2c35c648acff5a925f802
JoaquinKorol <joaquinkorol@gmail.com>
2024-08-22 11:40:38 -0300

    Cree clase HorariosDisponibles

---
+++ Models/HorariosDisponibles.cs
@@ -0,0 +1,19 @@
+﻿using NuGet.Protocol.Core.Types;
+using System.ComponentModel.DataAnnotations;
+
+namespace KoruCosmetica.Models
+{
+    public class HorariosDisponibles
+    {
+        [Key]
+        public int HorarioID { get; set; }
+        public TimeOnly HoraInicio { get; set; }
+        public TimeOnly HoraFin { get; set; }
+        public string? Estado { get; set; }
+
+        public int? DiaID {  get; set; }
+        public int? ProfesionalID {  get; set; }
+
+        public Profesionales? Profesional { get; set; }
+    }
+}
