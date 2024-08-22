e61d27192ce475135dfe790075029d13544b39e1
JoaquinKorol <joaquinkorol@gmail.com>
2024-08-22 11:42:58 -0300

    Cree Clase Vista Clientes Turnos

---
+++ Models/ViewModel/TurnosHorarios.cs
@@ -0,0 +1,9 @@
+﻿namespace KoruCosmetica.Models.ViewModel
+{
+    public class TurnosHorarios
+    {
+        public Turnos Turnos {  get; set; }
+        public HorariosDisponibles HorariosDisponibles { get; set; }
+
+    }
+}
