8a4700ad3ad424085659fd9921490ffd574f4d6b
JoaquinKorol <joaquinkorol@gmail.com>
2024-10-09 09:18:57 -0300

    Agregar ViewModel RestablecerContraseña

---
+++ Models/ViewModel/RestablecerContraseña.cs
@@ -0,0 +1,8 @@
+﻿namespace KoruCosmetica.Models.ViewModel
+{
+    public class RestablecerContraseña
+    {
+        public string NuevaContraseña { get; set; }
+        public string ConfirmarContraseña { get; set; }
+    }
+}
