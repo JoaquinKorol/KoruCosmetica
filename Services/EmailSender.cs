e96a01723e11de594d6d2893bd720baedc58692d
JoaquinKorol <joaquinkorol@gmail.com>
2024-10-09 10:12:25 -0300

    Agregue servicio EmailSender con SMTP

---
+++ Services/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using KoruCosmetica.Models;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+
+namespace KoruCosmetica.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly string _mail;
+        private readonly string _contraseña;
+
+        public EmailSender(IOptions<SmtpSettings> smtpSettings)
+        {
+            _mail = smtpSettings.Value.Mail;
+            _contraseña = smtpSettings.Value.Contraseña;
+            
+        }
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mail = _mail;
+            var pw = _contraseña;
+
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, pw)
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: mail,
+                                to: email,
+                                subject,
+                                message));
+
+        }
+    }
+}
