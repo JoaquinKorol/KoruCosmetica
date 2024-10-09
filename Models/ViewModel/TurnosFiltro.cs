d82b70060285325da9c21263306164f9faadbdfc
JoaquinKorol <joaquinkorol@gmail.com>
2024-10-09 09:20:28 -0300

    Agregar ViewModel TurnosFiltros para los filtros del admin

---
+++ Models/ViewModel/TurnosFiltro.cs
@@ -0,0 +1,11 @@
+﻿namespace KoruCosmetica.Models.ViewModel
+{
+    public class TurnosViewModel
+    {
+        public List<ClientesTurnos> Turnos { get; set; } = new List<ClientesTurnos>();
+        public DateOnly? FechaFiltro { get; set; }
+        public int? ClienteIDFiltro { get; set; }
+        public List<Clientes> Clientes { get; set; } = new List<Clientes>();
+    }
+
+}
