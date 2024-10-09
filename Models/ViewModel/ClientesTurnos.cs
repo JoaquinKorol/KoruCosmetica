8c909182ad83df7e4022d7a0cdd0390352eda821
JoaquinKorol <joaquinkorol@gmail.com>
2024-10-09 09:17:52 -0300

    Agregue el campo ID a la clase

--- Models/ViewModel/ClientesTurnos.cs
+++ Models/ViewModel/ClientesTurnos.cs
@@ -2,9 +2,10 @@
 {
     public class ClientesTurnos
     {
+        public int ID {  get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-        public DateTime Fecha { get; set; }
+        public DateOnly Fecha { get; set; }
         public TimeOnly Hora { get; set; }
         public string Servicio { get; set; }
 
