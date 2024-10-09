33ec31231d940e5f687eefe318459d57cc54f187
JoaquinKorol <joaquinkorol@gmail.com>
2024-10-09 08:58:13 -0300

    Agregue funcion restablecer contraseña

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -7,44 +7,38 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using KoruCosmetica.Data;
 using KoruCosmetica.Models;
+using Microsoft.CodeAnalysis.Scripting;
+using KoruCosmetica.Models.Requests;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using KoruCosmetica.Services;
+using System.Security.Policy;
+using KoruCosmetica.Models.ViewModel;
 
 namespace KoruCosmetica.Controllers
 {
     public class ClientesController : Controller
     {
         private readonly KoruCosmeticaContext _context;
+        private readonly string _secret;
+        private readonly IEmailSender _emailSender;
 
-        public ClientesController(KoruCosmeticaContext context)
+        public ClientesController(KoruCosmeticaContext context, IOptions<AppSettings> appSettings, IEmailSender emailSender)
         {
             _context = context;
+            _secret = appSettings.Value.Secret;
+            _emailSender = emailSender;
         }
 
-        // GET: Clientes
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Clientes.ToListAsync());
-        }
-
-        // GET: Clientes/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var clientes = await _context.Clientes
-                .FirstOrDefaultAsync(m => m.ClienteID == id);
-            if (clientes == null)
-            {
-                return NotFound();
-            }
-
-            return View(clientes);
-        }
 
         // GET: Clientes/Create
-        public IActionResult Create()
+        public IActionResult Registrar()
         {
             return View();
         }
@@ -52,109 +46,224 @@ namespace KoruCosmetica.Controllers
         // POST: Clientes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost("clientes/registrar")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ClienteID,Nombre,Apellido,Mail,Telefono")] Clientes clientes)
+        public async Task<IActionResult> Registrar([Bind("ClienteID,Nombre,Apellido,Mail,Telefono,Contraseña")] Clientes clientes)
         {
-            
             if (ModelState.IsValid)
             {
+                // Verificar si el usuario ya existe
+                var usuarioExistente = await _context.Clientes.FirstOrDefaultAsync(u => u.Mail == clientes.Mail);
+                if (usuarioExistente != null)
+                {
+                    ViewBag.ErrorMessage = "El Mail ya está registrado con otro usuario. Inicie sesión <a href='" + Url.Action("Login", "Clientes") + "'>aquí</a>.";
+                    return View(clientes); // Vuelve a mostrar la vista con el mensaje de error
+                }
+
+                // Hashear la contraseña antes de guardar
+                clientes.Contraseña = HashPassword(clientes.Contraseña);
+
+                // Agregar el nuevo cliente
                 _context.Add(clientes);
                 await _context.SaveChangesAsync();
-                TempData["ClienteID"] = clientes.ClienteID;
 
-                return RedirectToAction("Create", "TurnosHorarios");
+                var destinatario = clientes.Mail;
+                var asunto = "Confirmar registro Koru Cosmetica";
+                var mensaje = $"Hola {clientes.Nombre}, se ha registrado correctamente en koru cosmetica.";
+
+                await _emailSender.SendEmailAsync(destinatario, asunto, mensaje);
+
+                // Redirigir al índice de la página principal
+                return RedirectToAction("Index", "Home");
             }
+
+            // Si el modelo no es válido, volver a mostrar la vista
             return View(clientes);
         }
 
-        // GET: Clientes/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public IActionResult Login()
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var clientes = await _context.Clientes.FindAsync(id);
-            if (clientes == null)
-            {
-                return NotFound();
-            }
-            return View(clientes);
+            return View();
         }
 
-        // POST: Clientes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost("clientes/login")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ClienteID,Nombre,Apellido,Mail,Telefono")] Clientes clientes)
+        public async Task<IActionResult> Login([Bind("Mail,Contraseña")] Clientes clientes)
         {
-            if (id != clientes.ClienteID)
-            {
-                return NotFound();
-            }
+           
+            var usuario = await _context.Clientes.FirstOrDefaultAsync(u => u.Mail == clientes.Mail);
 
-            if (ModelState.IsValid)
+            if (usuario != null)
             {
+                if (BCrypt.Net.BCrypt.Verify(clientes.Contraseña, usuario.Contraseña))
+                {
+                    var token = GetToken(usuario); // Generar el token aquí
+
+                    // Configurar la cookie con el token
+                    var cookieOptions = new CookieOptions
+                    {
+                        HttpOnly = true, // Previene ataques XSS
+                        Secure = true,   // Asegúrate de que esto sea true en producción (requiere HTTPS)
+                        SameSite = SameSiteMode.Strict, // Previene ataques CSRF
+                        Expires = DateTime.UtcNow.AddHours(1) // Duración de la cookie
+                    };
+
+                    // Almacenar el token en la cookie
+                    HttpContext.Response.Cookies.Append("jwtToken", token, cookieOptions);
+
+                    // Redirigir al usuario
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "La contraseña es incorrecta.");
+                    return View(clientes);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "El usuario no existe.");
+                return View(clientes);
+            }
+        }
+
+        public IActionResult RecuperarContraseña()
+        {
+            return View();
+        }
+
+        public IActionResult MensajeEnviado()
+        {
+            return View();
+        }
+        public IActionResult ContraseñaActualizada()
+        {
+            return View(); // Asegúrate de que existe una vista ContraseñaActualizada
+        }
+
+        public IActionResult RestablecerContraseña()
+        {
+            // Aquí puedes agregar la lógica para restablecer la contraseña
+            return View(); // Asegúrate de que existe una vista RestablecerContraseña
+        }
+
+        public async Task<IActionResult> EnviarRecuperacion(Clientes clientes)
+        {
+            var usuarioExistente = await _context.Clientes.FirstOrDefaultAsync(u => u.Mail == clientes.Mail);
+            if (usuarioExistente != null)
+            {
+                var destinatario = clientes.Mail;
+                var asunto = "Recuperar Contraseña Koru Cosmetica";
+
+                // Crear un enlace de restablecimiento de contraseña (esto es solo un ejemplo)
+                var resetLink = Url.Action("RestablecerContraseña", "Clientes", new { email = clientes.Mail }, Request.Scheme);
+
+                var mensaje = $"Hola {usuarioExistente.Nombre}, se ha solicitado restablecer la contraseña en Koru Cosmetica. " +
+                              $"Por favor, haga clic en el siguiente enlace para restablecer su contraseña: <a href=\"{resetLink}\">Restablecer Contraseña</a>";
+
                 try
                 {
-                    _context.Update(clientes);
-                    await _context.SaveChangesAsync();
+                    await _emailSender.SendEmailAsync(destinatario, asunto, mensaje);
+                    return RedirectToAction("MensajeEnviado", "Clientes");
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (Exception ex)
                 {
-                    if (!ClientesExists(clientes.ClienteID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    // Manejo de errores en el envío del correo
+                    // Aquí podrías registrar el error o mostrar un mensaje al usuario
+                    ModelState.AddModelError(string.Empty, "Hubo un error al enviar el correo. Intente nuevamente más tarde.");
+                    return View(clientes);
                 }
-                return RedirectToAction(nameof(Index));
             }
-            return View(clientes);
+            else
+            {
+                // El usuario no existe
+                ModelState.AddModelError(string.Empty, "No se encontró ningún usuario con ese correo electrónico.");
+                return View(clientes);
+            }
+
+
         }
 
-        // GET: Clientes/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        [HttpPost]
+        public async Task<IActionResult> ActualizarContraseña(string email, Clientes cliente, CambiarContraseña model)
         {
-            if (id == null)
+            // Asegúrate de que el cliente contenga la nueva contraseña
+            
+            // Busca el usuario por su correo electrónico
+            var usuario = await _context.Clientes.FirstOrDefaultAsync(u => u.Mail == email);
+            if (usuario != null)
             {
-                return NotFound();
+                // Actualiza la contraseña
+                if(model.NuevaContraseña == model.ConfirmarContraseña)
+                {
+                    usuario.Contraseña = HashPassword(model.NuevaContraseña);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction("ContraseñaActualizada", "Clientes");
+                }
+                else 
+                {
+                    ModelState.AddModelError(string.Empty, "Las contraseñas no coinciden.");
+                    return View(model);
+                }
+                
             }
 
-            var clientes = await _context.Clientes
-                .FirstOrDefaultAsync(m => m.ClienteID == id);
-            if (clientes == null)
-            {
-                return NotFound();
-            }
-
-            return View(clientes);
+            // Si no se encuentra el usuario, redirige a la página de error
+            return RedirectToAction("Error", "Home");
         }
 
-        // POST: Clientes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> Logout()
         {
-            var clientes = await _context.Clientes.FindAsync(id);
-            if (clientes != null)
-            {
-                _context.Clientes.Remove(clientes);
-            }
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            // Eliminar las cookies manualmente
+            HttpContext.Response.Cookies.Delete(".AspNetCore.Cookies");
+            HttpContext.Response.Cookies.Delete("jwtToken");
+
+            return RedirectToAction("Index", "Home");
         }
 
-        private bool ClientesExists(int id)
+
+        private string HashPassword(string password)
         {
-            return _context.Clientes.Any(e => e.ClienteID == id);
+            
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+
+        private string GetToken(Clientes clientes)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secret);
+
+            // Configura los claims que serán parte del token
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, clientes.ClienteID.ToString()),
+                new Claim(ClaimTypes.Email, clientes.Mail),
+                new Claim("nombre", clientes.Nombre)
+            };
+
+            // Agregar el rol de Admin si el email es el específico
+            if (clientes.Mail == "detefi3132@heweek.com")
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+            }
+            else
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "User"));
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(10), // Ajusta la expiración según sea necesario
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
