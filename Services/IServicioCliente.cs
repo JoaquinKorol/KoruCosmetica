98943a16e1634e3667f4afde24a41c97be2bd81d
JoaquinKorol <joaquinkorol@gmail.com>
2024-10-15 15:18:40 -0300

    Agregar interfaz IServicioCliente para la lógica de negocio de los clientes

---
+++ Services/IServicioCliente.cs
@@ -0,0 +1,12 @@
+﻿using KoruCosmetica.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoruCosmetica.Services
+{
+    public interface IServicioCliente
+    {
+        Task<string> RegistrarAsync(Clientes clientes);
+        Task<(string? mensaje, string? token, bool exitoso)> LoginAsyc(Clientes clientes);
+        Task LogoutAsync(HttpContext httpContext);
+    }
+}
