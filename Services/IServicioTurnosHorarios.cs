0fe4239769f7fb2ed77b0c88d3b1d9aa36acc4ca
JoaquinKorol <joaquinkorol@gmail.com>
2024-10-15 15:23:51 -0300

    Agregar interfaz IServicioTurnosHorarios para la lógica de negocio de los horarios de turnos

---
+++ Services/IServicioTurnosHorarios.cs
@@ -0,0 +1,11 @@
+﻿using KoruCosmetica.Models.ViewModel;
+
+namespace KoruCosmetica.Services
+{
+    public interface IServicioTurnosHorarios
+    {
+        Task<string[]> ObtenerHorariosDisponibles(DateTime fecha);
+        Task<bool> SacarTurno(TurnosHorarios viewModel, string clienteIdClaim);
+        Task<bool> CancelarTurno(int id);
+    }
+}
