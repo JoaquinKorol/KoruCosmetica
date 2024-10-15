12eefa11c5b56fc5d9770fbd753a93ad29982ef2
JoaquinKorol <joaquinkorol@gmail.com>
2024-10-15 15:23:04 -0300

    Agregar interfaz IServicioRecuperar para la recuperación de contraseñas

---
+++ Services/IServicioRecuperar.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Core;
+using KoruCosmetica.Models;
+using KoruCosmetica.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Policy;
+
+namespace KoruCosmetica.Services
+{
+    public interface IServicioRecuperar
+    {
+        Task<bool> EnviarRecuperacionAsync(Clientes clientes, HttpContext httpContext);
+        Task<(bool resultado, string mensaje)> ActualizarContraseña(string mail, CambiarContraseña model);
+
+
+    }
+}
