71c07f17425c040255cdf9f9f7b1b9e26e4b7753
JoaquinKorol <joaquinkorol@gmail.com>
2024-10-15 15:17:07 -0300

    Agregar interfaz IServicioAdminTurnos

---
+++ Services/IServicioAdminTurno.cs
@@ -0,0 +1,9 @@
+﻿using KoruCosmetica.Models.ViewModel;
+
+namespace KoruCosmetica.Services
+{
+    public interface IServicioAdminTurno
+    {
+        List<ClientesTurnos> ObtenerTurnosFiltrados(string nombreBuscar, DateOnly? fechaDesde, DateOnly? fechaHasta, int? servicioBuscar);
+    }
+}
